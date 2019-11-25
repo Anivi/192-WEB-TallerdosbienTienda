0afd70b654b34cd995d9b4c028eae44ff8054038
anivi <anivi98@hotmail.com>
2019-11-25 08:09:34 -0500

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express'),
+    engines = require('consolidate');
+
+var app = express();
+
+
+app.engine('hbs', engines.handlebars);
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+//---------------//
+
+//Index
+app.get( '/', ( req, res ) => {
+    res.render('index');
+});
+
+//Tienda
+app.get('/tienda', (req,res) => {
+    res.render('tienda');
+});
+
+//Carrito
+app.get('/carrito', (req,res) => {
+    res.render('carrito');
+});
+
+// Producto
+app.get('/producto', (req,res) => {
+    res.render('producto');
+});
+
+    
+app.listen(5000);
